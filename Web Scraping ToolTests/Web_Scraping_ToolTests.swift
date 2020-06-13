7ed2744e2a9db6b697a334516f3865a8bf8501c8
256hax <256hax@git>
2020-06-13 17:52:02 +0900

    init: use Master-Detail App template

---
+++ Web Scraping ToolTests/Web_Scraping_ToolTests.swift
@@ -0,0 +1,34 @@
+//
+//  Web_Scraping_ToolTests.swift
+//  Web Scraping ToolTests
+//
+//  Created by user on 2020/06/13.
+//  Copyright © 2020 256hax. All rights reserved.
+//
+
+import XCTest
+@testable import Web_Scraping_Tool
+
+class Web_Scraping_ToolTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
